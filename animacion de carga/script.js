46466bc2244ed06ff572b594670434c809578d40
developerestefani <developercanasestefani@gmail.com>
2023-12-21 12:13:44 -0400

    style(front): Add all styles without function

---
+++ animacion de carga/script.js
@@ -0,0 +1,11 @@
+// Se carga con la clase los dos contenedores del sitio web
+
+// Se declara una variable local en 0
+
+// se tiene que utiliar una llamada a una funcion por intervalo de duracion de 30milisegundos
+
+// Se crea la funcion de dara la animacion al contador
+
+    // Primero de declara un contador
+
+    // Luego se declara la desicion siendo que el contador es mayor a 99 
